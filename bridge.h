0a7b3d8562a68bc88ccf4bd2bae558f0bf048791
ericmseitz <eric.seitz@gmail.com>
2015-11-30 18:32:16 -0800

    Add AFNetworking and bridge.h file

---
+++ bridge.h
@@ -0,0 +1,13 @@
+//
+//  bridge.h
+//  webServicesExample
+//
+//  Created by Eric M. Seitz on 11/30/15.
+//  Copyright © 2015 Eric M. Seitz. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+#import "AFNetworking/AFNetworking.h"
+
+#endif /* bridge_h */
